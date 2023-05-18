4921c4657949e4086764830c3f7df9f13dccede4
Eduard Ruiz Munné <eduardrm2000@gmail.com>
2023-05-18 09:58:57 +0200

    Recombination operator

--- operators.py
+++ operators.py
@@ -16,6 +16,9 @@ class Mutation:
 
 
 class Crossover:
+    def __init__(self, xover_rate: float = 0.4):
+        self.xover_rate = xover_rate
+
     def do(self, pop, mating_pop_ids):
         print("CROSSOVER")
         pop_size = len(pop)
@@ -24,8 +27,17 @@ class Crossover:
             shuffle(shuffled_ids)
         else:
             shuffled_ids = mating_pop_ids
-        # import pdb; pdb.set_trace()
-        return pop[shuffled_ids]
+        n_features = len(pop[0])
+        for i in range(0, pop_size, 2):
+            number_xover = int(self.xover_rate * n_features)  # Number of crossover points
+            xover_points = np.random.choice(list(range(n_features)), number_xover, replace=False)  # Randomly select the crossover points
+            values_xover = pop[shuffled_ids[i]][xover_points]  # Values from the first parent to be swapped in the second parent
+            k = 0  # Counter for the values to be swapped
+            for j in range(n_features):
+                if pop[shuffled_ids[i+1]][j] in values_xover:  # If the value is in the first parent, swap it with the value from the second parent
+                    pop[shuffled_ids[i+1]][j] = values_xover[k]
+                    k += 1
+        return pop
 
 
 # class SurrogatePopulation(Population, BasePopulation):
